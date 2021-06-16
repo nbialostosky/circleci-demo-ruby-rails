8efea01171f904fbe13b43691ea490fb7b4b2470
Nick Bialostosky <nick.bialostosky@circleci.com>
2021-06-16 14:28:10 -0500

    test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe PostsController, type: :controller do
     it 'returns a success response' do
       Post.create! valid_attributes
       get :index, params: {}, session: valid_session
-      expect(response).to be_successful
+      expect(response).to be_failure
     end
   end
 
